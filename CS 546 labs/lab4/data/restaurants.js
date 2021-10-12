3804355361a2ac463440afdc6b87b95a781fc0a8
Aditya Parab <adityaparab04@gmail.com>
2021-10-11 23:01:52 -0400

    lab 4 final commit

--- CS 546 labs/lab4/data/restaurants.js
+++ CS 546 labs/lab4/data/restaurants.js
@@ -4,28 +4,66 @@ let { ObjectId } = require('mongodb');
 
 //create a newRestaurant object with the given parameters
 const create = async function create(name, location, phoneNumber, website, priceRange, cuisines, overallRating, serviceOptions){    
-    if(!name || typeof name !== 'string' || !name.replace(/\s/g, "").length){
-        throw `Please enter a valid input name as a string`
-    }
+    //arguments length check
+    if (arguments.length !== 8) throw new Error ("Enter all the properties of a restaurant");
+
+    //Error handling for name
+    if(!name || typeof name !== 'string' || !name.replace(/\s/g, "").length) throw new Error ("Please enter a valid input name as a string");
+    
+    //Error handling for location
     if(!location || typeof location !== 'string' || !location.replace(/\s/g, "").length){
-        throw `Please enter a valid input location as a string`
+        throw new Error ("Please enter a valid input location as a string");
     }
+    
+    //Error handling for phoneNumber
     if(!phoneNumber || typeof phoneNumber !== 'string' || !phoneNumber.replace(/\s/g, "").length){
-        throw `Please enter a valid input phoneNumber as a string`
+        throw new Error ("Please enter a valid input phoneNumber as a string")
     }
+    if(phoneNumber.match(/^\d{3}[-]\d{3}[-]\d{4}$/) === null){
+        throw new Error ("Please enter a valid phone number")
+    }
+    //Error handling for website
     if(!website || typeof website !== 'string' || !website.replace(/\s/g, "").length){
-        throw `Please enter a valid input website as a string`
+        throw new Error ("Please enter a valid URL in string format")
     }
-    if(!priceRange || typeof priceRange !== 'string' || !priceRange.replace(/\s/g, "").length){
-        throw `Please enter a valid input priceRange as a string`
-    }
-    if(!cuisines || Array.isArray(cuisines) !== true || cuisines.length === 0){
-        throw `Please enter cuisines as an array`
-    }
-    if(!overallRating || typeof overallRating !== 'number'){
-        throw `Please enter a valid input name as a string`
+    if(website.match(/^(http:\/\/www\.|https:\/\/www\.)[a-zA-z0-9\_-]{5,}(\.com)$/g) === null){
+        throw new Error ("Please enter a valid URL in string format")
     }
 
+    //Error handling for priceRange
+    if(!priceRange || typeof priceRange !== 'string' || !priceRange.replace(/\s/g, "").length){
+        throw new Error ("Please enter a valid input priceRange as a string")
+    }
+    if(priceRange.match(/^[$]{1,4}$/g) === null){
+        throw new Error("Enter valid price range between $ to $$$$")
+    }
+
+    //Error handling for cuisines
+    if(!cuisines || Array.isArray(cuisines) !== true || cuisines.length === 0 ){
+        throw new Error ("Please enter cuisines as an array of atleast length 1 with valid input string")
+    }
+    cuisines.forEach(cuisine => {
+        if(typeof cuisine != 'string' || !cuisine.replace(/\s/g, "").length) throw new Error ("enter valid string for cuisine")
+    });
+
+    //Error handling for overallRating
+    if(!overallRating || typeof overallRating !== 'number'){
+        throw new Error ("Please enter a valid input name as a string")
+    }
+    if(overallRating < 0 || overallRating > 5){
+        throw new Error ("Rating should be in between 0 - 5")
+    }
+
+    //Error handling for serviceOptions
+    if(!serviceOptions || typeof serviceOptions !== 'object' || Object.keys(serviceOptions).length !== 3){
+        throw new Error ("Please enter a valid serviceOption")
+    }
+    if(serviceOptions.hasOwnProperty('dineIn', 'takeOut', 'delivery') !== true) {
+        throw new Error ("Please enter right service options with boolean values")
+    }
+    if (typeof serviceOptions.dineIn !== 'boolean' || typeof serviceOptions.takeOut !== 'boolean' || typeof serviceOptions.delivery !== 'boolean') {
+        throw new Error ("enter valid key and value for serviceOptions")
+    }
     let newRestaurant ={
         name: name,
         location: location,
@@ -36,13 +74,22 @@ const create = async function create(name, location, phoneNumber, website, price
         overallRating: overallRating,
         serviceOptions: serviceOptions
     };
-    const restaurantsCollection = await restaurants();
-    const insertNewRestaurant = await restaurantsCollection.insertOne(newRestaurant);
+    const restaurantCollection = await restaurants();
+    
+    let arrayOfRestaurants = [];
+    arrayOfRestaurants = await restaurantCollection.find({}).toArray();
+    for (let i = 0; i < arrayOfRestaurants.length; i++){
+        if(arrayOfRestaurants[i].name.toLowerCase() === newRestaurant.name.toLowerCase() && arrayOfRestaurants[i].location.toLowerCase() === newRestaurant.location.toLowerCase() && arrayOfRestaurants[i].phoneNumber === newRestaurant.phoneNumber){
+            throw new Error ("Same Restaurant already added")
+        }
+    }
+    const insertNewRestaurant = await restaurantCollection.insertOne(newRestaurant);
     if (insertNewRestaurant.insertedCount === 0){
-        throw `Could not add restaurant`
+        throw new Error ("Could not add restaurant")
     }
     const newId = insertNewRestaurant.insertedId;
-    const restaurant = await this.get(newId);
+    newIdStringFormat = newId.toString()
+    const restaurant = await this.get(newIdStringFormat);
     return restaurant;
 }
 
@@ -61,17 +108,29 @@ const getAll = async function getAll(){
         allRestaurant[i] = obj;
         i+=1
     });
-    return allRestaurant
+    if(allRestaurant.length === 0){
+        console.log("No restaurants in Database")
+    }
+    return allRestaurant;
 }
 
 
 //Find restaurant by ID
 const get = async function get(id){
+    if(!id){
+        throw new Error ("Please enter an id in string format")
+    }
+    if(typeof id !== 'string' || !id.replace(/\s/g, "").length){
+        throw new Error ("Entered id not in string format or contains blank spaces only")
+    }
+    if(ObjectId.isValid(id) === false){
+        throw new Error ("Entered Id is not a valid object Id")
+    }
     const restaurantCollection = await restaurants();
     let parseId = ObjectId(id)
     const restaurant = await restaurantCollection.findOne({ _id: parseId});
     if (restaurant === null){
-        throw `No restaurant with that id`
+        throw new Error ("No restaurant with that id")
     }
     restaurant._id = restaurant._id.toString();
     return restaurant;
@@ -79,32 +138,70 @@ const get = async function get(id){
 
 //remove restaurant by id
 const remove = async function remove(id){
+    if(!id){
+        throw new Error ("Please enter an id in string format")
+    }
+    if(typeof id !== 'string' || !id.replace(/\s/g, "").length){
+        throw new Error ("Entered id not in string format or contains blank spaces only")
+    }
+    if(ObjectId.isValid(id) === false){
+        throw new Error ("Entered Id is not a valid object Id")
+    }
+    
     const restaurantCollection = await restaurants();
     let parseId = ObjectId(id);
     const deleteRestaurant = await restaurantCollection.deleteOne({_id: parseId})
-    if (deleteRestaurant.deleteCount === 0){
-        throw `Couldn't delete the id`
+    if (deleteRestaurant.deletedCount === 0){
+        throw new Error ("Couldn't delete the id because it is not present in DB")
     }
 }
 
 //update the website of the restaurant
 const rename = async function rename(id, newWebsite){
+    if(!id){
+        throw new Error ("Please enter an id in string format")
+    }
+    if(typeof id !== 'string' || !id.replace(/\s/g, "").length){
+        throw new Error ("Entered id not in string format or contains blank spaces only")
+    }
+    if(ObjectId.isValid(id) === false){
+        throw new Error ("Entered Id is not a valid object Id")
+    }
+
+    if(!newWebsite){
+        throw new Error ("new website is not provided")
+    }
+    if(typeof newWebsite !== 'string' || !newWebsite.replace(/\s/g, "").length){
+        throw new Error ("Entered id not in string format or contains blank spaces only")
+    }
+    if(newWebsite.match(/^(http:\/\/www\.|https:\/\/www\.)[a-zA-z0-9\_-]{5,}(\.com)$/g) === null){
+        throw new Error ("Please enter a valid URL in string format")
+    }
+
     const restaurantCollection = await restaurants();
+    
+    let arrayOfRestaurants = [];
+    arrayOfRestaurants = await restaurantCollection.find({}).toArray();
+    for (let i = 0; i < arrayOfRestaurants.length; i++){
+        if(arrayOfRestaurants[i]._id.toString() === id){
+            if(arrayOfRestaurants[i].website.toLowerCase() === newWebsite.toLowerCase()){
+                throw new Error ("Same website link entered")
+            }
+        }
+    }
+    let parseId = ObjectId(id);
     let updateWebsite = {
         website: newWebsite
     };
-    let parseId = ObjectId(id);
     const renamedWebsite = await restaurantCollection.updateOne(
         { _id: parseId },
         { $set: updateWebsite }
     );
     if (renamedWebsite.modifiedCount === 0){
-        throw `Couldn't update restaurant successfully`
+        throw new Error ("Couldn't update website of the restaurant, Id not found")
     }
-    return await this.get(parseId);
+    return await this.get(id);
 }
-
-
 
 
 module.exports = {
--- CS 546 labs/lab4/index.js
+++ CS 546 labs/lab4/index.js
@@ -2,45 +2,74 @@ const restaurants = require('./data/restaurants');
 const connection = require('./config/mongoConnection');
 
 async function main(){
+    //create a restaurant and log
+        const test1 = await restaurants.create("Curry On", "Jersey City, New Jersey", "123-768-0870", "https://www.curryOnmyPlate.com", "$$$", ["Indian", "Continental"], 5, {dineIn: true, takeOut: true, delivery: false})
+        const test2 = await restaurants.get(test1._id)
+        console.log(test2)
+
+    //create another restaurant
+        const test3 = await restaurants.create("Chef Of India", "Jersey City, New Jersey", "123-786-9870", "https://www.ChefOfIndia.com", "$$$", ["Indian", "Desi"], 5, {dineIn: true, takeOut: true, delivery: true})
+
+    //log all restaurants
+        const test4 = await restaurants.getAll();
+        console.log(test4)
+
+    //create third restaurant and log
+        const test5 = await restaurants.create("Republic Pizza", "Hoboken, New Jersey", "214-768-0870", "https://www.PizzaRepublic.com", "$$", ["Italian"], 5, {dineIn: true, takeOut: false, delivery: false})
+        const test6 = await restaurants.get(test5._id)
+        console.log(test6)
+
+    //Rename the first restaurant website and log
+        const test7 = await restaurants.rename(test1._id, "http://www.KurryOn.com");
+        const test8 = await restaurants.get(test7._id)
+        console.log(test8)
     
-    try{
-        console.log(await restaurants.create(" The docks", "Mumbai City, Maharashtra", "123-456-7890", "http://www.saffronlounge.com", " ", ["Indian", "Desi"], 3, {dineIn: true, takeOut: true, delivery: false}))
-    }catch(e){
+    //Remove 2nd restaurant
+        const test9 = await restaurants.remove(test3._id);
+
+    //query all restaurants and log them
+        const test10 = await restaurants.getAll()
+        console.log(test10)
+
+    //Try to create a restaurant with bad input parameters to make sure it throws errors
+    try {
+        const test11 = await restaurants.create("Curry On", "Jersey heights, New Jersey", "123-768-0870", "htt://www.curryOnmyPlate.com", "$$$", ["Indian", "Continental"], 5, {dineIn: true, takeOut: true, delivery: false});
+    } catch(e) {
         console.log("Got an error!");
         console.log(e);
     }
 
-    // try{
-    //     console.log(await restaurants.get("61636fcf00fa72816a25ae07"))
-    // }catch(e){
-    //     console.log("Got an error!");
-    //     console.log(e);
-    // }
+    //Try to remove a restaurant that does not exist to make sure it throws errors.
+    try {
+        const test12 = await restaurants.remove(test3._id);
+    } catch(e) {
+        console.log("Got another error!");
+        console.log(e);
+    }
 
-    // try{
-    //     const allResturants = await restaurants.getAll();
-    //     console.log(allResturants);
-    // }catch(error){
-    //     console.log("Got an error!");
-    //     console.log(error);
-    // }
-    
-    // try{
-    //     await restaurants.remove("61637610258456c5967b941b");
-    //     console.log("removed restaurant successfully")
-    // }catch(error){
-    //     console.log("Got an error!");
-    //     console.log(error);
-    // }
+    //Try to rename a restaurant that does not exist to make sure it throws errors.
+    try {
+        const test13 = await restaurants.remove(test3._id);
+    } catch(e) {
+        console.log("Got one more error!");
+        console.log(e);
+    }
 
-//    try{
-//         console.log(await restaurants.rename("6163586dab8c77da9452b254", "http://www.thesaffronlounge.com"));
-//     }catch(error){
-//         console.log("Got an error!");
-//         console.log(error);
-//     } 
-    
+    //Try to rename a restaurant passing in invalid data for the parameter to make sure it throws errors.
+    try {
+        const test14 = await restaurants.rename(test3._id, "http://www.testingtofail.com");
+    } catch(e) {
+        console.log("Got more errors!");
+        console.log(e);
+    }
 
+    //Try getting a restaurant by ID that does not exist to make sure it throws errors.
+    try {
+        const test15 = await restaurants.get("8982398432");
+    } catch(e) {
+        console.log("Got many more errors!");
+        console.log(e);
+    }
     
     const db = await connection.connectToDb();
     await connection.closeConnection();
