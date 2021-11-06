7e7716c7228a24b62b1cee7fdbb9259daaf1db16
adityaparab04 <adityaparab04@gmail.com>
2021-11-06 15:39:29 -0400

    folders created

---
+++ DB/db_proposal.js
@@ -0,0 +1,69 @@
+userCollections:[
+    "user1":{
+        "userId":"71982708as315sesde34614",
+        "userName": "Deku",
+        "password": "fdshdaiuh",
+        "age": 22,
+        "favorite":["My hero acadeimia", "Demon Slayer"],
+        "likes": 15,
+        "dislikes": 34,
+        "reviews": [{"reviewId": "91782708gs356sesde34614",
+                     "review":"This is the best anime ever"},
+                     {"reviewId": "41782706hk358sasde34614",
+                     "review":"This is the best anime ever"}],
+        "comments": [{"commentId":"74582708as356sehde34614",
+                        "comment":"I don't agree with you on that"}]
+    }
+]
+
+animeCollections:[
+    "Anime1":{
+        "animeId": "45982708as315sesde34614",
+        "animeName": "My hero academia",
+        "englishName": "Boku no hero academia",
+        "type": "series",
+        "episodeCount": 45,
+        "status": "ongoing",
+        "genre": "adventure scifi",
+        "overallRating": 5,
+        "releasedYear": "2010",
+        "duration": "22 mins",
+        "synopsis": "At UA Academy, not even a violent attack can disrupt their most prestigious event: the school sports festival. Renowned across Japan, this festival is an opportunity for aspiring heroes to showcase their abilities, both to the public and potential recruiters.",
+        "reviews": [{"reviewId": "91782708gs356sesde34614",
+                     "review":"This is the best anime ever"},
+                     {"reviewId": "41782706hk358sasde34614",
+                     "review":"The story line went bad from the 2nd season"}],
+    },
+]
+
+reviewsCollection:[
+    "Review1":{
+        "reviewId": "91782708gs356sesde34614",
+        "userId": "71982708as315sesde34614",
+        "date": "MM/DD/YYYY",
+        "rating": 5,
+        "review": "The story line went bad from the 2nd season",
+        "likes": 6,
+        "dislikes": 2,
+        "commentId": "s17885083s315sesde34614"
+    },
+    "Review2":{
+        "reviewId": "41782706hk358sasde34614",
+        "userId": "71982708as315sesde34614",
+        "date": "MM/DD/YYYY",
+        "rating": 5,
+        "review": "This is the best anime ever",
+        "likes": 4,
+        "dislikes": 2,
+        "commentId": "s148a5052s315sesde34614"
+    },
+]
+
+commentsCollection:[
+    "comment1":{
+        "commentId":"s148a5052s315sesde34614",
+        "userId": "71982708as315sesde34614",
+        "comment": "I did not agree with you",
+        "date": "MM/DD/YYYY"
+    }
+]
